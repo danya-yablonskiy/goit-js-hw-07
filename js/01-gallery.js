08fd6f23079016ecdd181d2d5fd9b0544a6b1ef9
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-03-01 14:59:35 +0200

    01-gallery.js

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const gallery = document.querySelector('.gallery');
+const createGallery = createImageItems();
+
+gallery.insertAdjacentHTML('beforeend', createGallery);
+
+function createImageItems (){
+    return galleryItems.map(({preview, original, description}) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt='${description}'
+        />
+      </a>
+    </div>`).join('')
+}
+
+gallery.addEventListener('click', onModalClick);
+
+
+function onModalClick(e){
+    e.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
+instance.show();
+
+window.addEventListener('keydown', onEscClose);
+function onEscClose(e){
+if (e.code==="Escape") {
+    instance.close();
+    window.removeEventListener('keydown', onEscClose);
+}
+}
+}
+
+
