55c4607704ec5216fa93d4d5c50b2bdf0e42b4a6
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-03-02 18:40:18 +0200

    test.js

---
+++ js/test.js
@@ -0,0 +1,8 @@
+function getExtremeElements(array) {
+    // Change code below this line
+const a = [array[0], array[array.length-1]];
+  return a;
+  
+    // Change code above this line
+  }
+  console.log(getExtremeElements[1,24,2,21])  
